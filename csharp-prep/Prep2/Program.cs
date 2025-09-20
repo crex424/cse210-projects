d51866c5b1cc8df8b1d270fbc6b09913c77e74c3
Campbell Rex <campbellrex@outlook.com>
2025-09-20 16:59:13 -0600

    Added prompt and functional to get grade percent and convert to letter grade. Displays whether they pass.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Create a variable to store letter grade.
+        string letter_grade;
+
+        // Prompt user for their grade percentage and store it.
+        Console.WriteLine("What is your grade percentage? ");
+        string user_input = Console.ReadLine();
+
+        // Covert the users grade percent into a integer.
+        int grade_percent = int.Parse(user_input);
+        // Check what letter grade the have, and store it in the letter_grade variable.
+        if (grade_percent >= 90)
+        {
+            letter_grade = "A";
+        }
+        else if (grade_percent >= 80)
+        {
+            letter_grade = "B";
+        }
+        else if (grade_percent >= 70)
+        {
+            letter_grade = "C";
+        }
+        else if (grade_percent >= 60)
+        {
+            letter_grade = "D";
+        }
+        else
+        {
+            letter_grade = "D";
+        }
+        // Display letter grade.
+        Console.WriteLine($"Your letter grade is {letter_grade}.");
+
+        // Check whether the user passed their class.
+        if (grade_percent >= 70)
+        {
+            Console.WriteLine("Congrats, you have passed.");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time. Work hard and nothing is out of your reach.");
+        }
     }
 }

ddb9832091f3ded613e56e9ed107d1f64941debe
Campbell Rex <campbellrex@outlook.com>
2025-09-25 13:51:19 -0600

    Wrote functionality for adding numbers to a list and getting the average, largest, and sum

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> list_n = new List<int>();
+        bool keep_going = true;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (keep_going == true)
+        {
+            Console.Write("Enter Number: ");
+            int number = int.Parse(Console.ReadLine());
+            if (number == 0)
+            {
+                keep_going = false;
+            }
+            else
+            {
+                list_n.Add(number);
+            }
+        }
+        decimal sum = 0;
+        int largest = 0;
+        foreach (int n in list_n)
+        {
+            if (n > largest)
+            {
+                largest = n;
+            }
+            sum += n;
+        }
+        
+        decimal average = sum / list_n.Count;
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }

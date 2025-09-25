31c918d36857e91ae801e35edce38e77a44c81e1
Campbell Rex <campbellrex@outlook.com>
2025-09-25 13:26:16 -0600

    Added functionality for number guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        int magic_num = int.Parse(Console.ReadLine());
+        bool guess_correct = false;
+        while (guess_correct != true)
+        {
+            Console.Write("What is your guess? ");
+            int guess = int.Parse(Console.ReadLine());
+
+            if (guess == magic_num)
+            {
+                guess_correct = true;
+                Console.WriteLine("You Guessed It!");
+            }
+            else if (guess > magic_num)
+            {
+                Console.WriteLine("Lower.");
+            }
+            else
+            {
+                Console.WriteLine("Higher.");
+            }
+        }
+
     }
 }

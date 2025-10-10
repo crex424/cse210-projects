3baff2e513cc25606ab87a7a2dd4b1c1e6c272a4
Campbell Rex <campbellrex@outlook.com>
2025-10-09 21:27:48 -0600

    Added functionality for load and save methods

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -10,16 +10,63 @@ public class Journal
             _entries[i].Display();
         }
     }
-    public bool SaveJournal()
+    public void SaveJournal()
     {
-        var writer = new StreamWriter("journal_storage.csv");
-
-        return true;
+        Console.WriteLine("What is the file name?");
+        string file_name = Console.ReadLine();
+        
+        using (var writer = new StreamWriter(file_name, false))
+        {
+            foreach (Entry entry in _entries)
+            {
+                entry._entry = entry._entry.Replace(',', '|');
+                entry._entry = entry._entry.Replace('"', '~');
+                writer.WriteLine($"{entry._date}, {entry._prompt}, {entry._entry}");
+            }
+        }
     }
 
     public Journal LoadJournal()
     {
         Journal loaded_journal = new Journal();
+
+        bool is_found = false;
+        while (is_found == false)
+        {
+            Console.WriteLine("What is the file name?");
+            string file_name = Console.ReadLine();
+            try
+            {
+                using (StreamReader reader = new StreamReader(file_name))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] split_line = line.Split(',');
+                        string date = split_line[0];
+                        string prompt = split_line[1];
+                        string entry = split_line[2];
+
+                        entry = entry.Replace('|', ',');
+                        entry = entry.Replace('~', '"');
+
+                        Entry new_entry = new Entry();
+                        new_entry._date = date;
+                        new_entry._prompt = prompt;
+                        new_entry._entry = entry;
+
+                        loaded_journal._entries.Add(new_entry);
+
+                        is_found = true;
+                    }
+                }
+            }    
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found");
+            }
+        
+        } 
         return loaded_journal;
     }
 

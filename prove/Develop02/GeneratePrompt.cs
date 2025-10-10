6b4296d979b5115c79ec2fe9e52ed99452830575
Campbell Rex <campbellrex@outlook.com>
2025-10-09 20:30:14 -0600

    Replaced hard coded random length with dynamic count

--- prove/Develop02/GeneratePrompt.cs
+++ prove/Develop02/GeneratePrompt.cs
@@ -6,10 +6,12 @@ public class GeneratePrompt
     public string GetPrompt()
     {
         Random rand = new Random();
-        int random_num = rand.Next(5);
+        int random_num = rand.Next(_prompts.Count());
         return _prompts[random_num];
     }
-
+    /*
+    Displays all the prompts from a the prompt list.
+    */
     public void Display()
     {
         for (int i = 0; i < _prompts.Count; i++)

e600fe50ad869d5dd83f545e8500bec68830e6d3
Campbell Rex <campbellrex@outlook.com>
2025-10-09 21:27:20 -0600

    Removed depricated method

--- prove/Develop02/GeneratePrompt.cs
+++ prove/Develop02/GeneratePrompt.cs
@@ -9,15 +9,4 @@ public class GeneratePrompt
         int random_num = rand.Next(_prompts.Count());
         return _prompts[random_num];
     }
-    /*
-    Displays all the prompts from a the prompt list.
-    */
-    public void Display()
-    {
-        for (int i = 0; i < _prompts.Count; i++)
-        {
-            Console.WriteLine($"{_prompts}");
-        }
-
-    }
 }

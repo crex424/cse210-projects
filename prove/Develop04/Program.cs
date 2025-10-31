ede329913e9fd7f8f2ba500e0dba783a8bf740b8
Campbell Rex <campbellrex@outlook.com>
2025-10-30 21:25:39 -0600

    Added program functionality

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        MathAssignment assign1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(assign1.GetSummary());
+        Console.WriteLine(assign1.GetHomework());
+
+        WritingAssignment writing = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }

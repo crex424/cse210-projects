a4022084b6ec4f906c0b19be19e6e906db3ec4ce
Campbell Rex <campbellrex@outlook.com>
2025-11-01 18:44:59 -0600

    Moved learning assignment to the correct location

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment assign1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(assign1.GetSummary());
+        Console.WriteLine(assign1.GetHomework());
+
+        WritingAssignment writing = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }

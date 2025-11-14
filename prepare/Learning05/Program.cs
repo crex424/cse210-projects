cc6c27c70afe8d3e5afa743296fd81c28b8ab46b
Campbell Rex <campbellrex@outlook.com>
2025-11-13 21:21:55 -0700

    Created classes and functionality for shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("red", 2.5);
+        Rectangle rectangle = new Rectangle("blue", 5.5, 2.6);
+        Circle circle = new Circle("green", 6.5);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+        }
     }
 }
